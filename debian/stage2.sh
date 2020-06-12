d79973dc63a7a04f5763c617a816dbb5592d1bb7
Mochamad Arifin <klampok.child@gmail.com>
2020-06-12 13:27:33 +0700

    invoke service

--- debian/stage2.sh
+++ debian/stage2.sh
@@ -3,4 +3,13 @@
 echo "debian" > /etc/hostname
 echo "127.0.0.1     debian" >> /etc/hosts
 
+#boot.d run by /etc/rc.local
+mkdir /etc/boot.d
+cat <<EOF > /etc/boot.d/services
+#!/bin/bash
+
+service ssh start
+EOF
+
+chmod +x  /etc/boot.d/services
 apt clean
